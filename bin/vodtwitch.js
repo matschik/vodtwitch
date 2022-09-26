24e2f0016c6608eaf939e1903ed8c14eeca98866
Mathieu Schimmerling <mathieu.schimmerling@protonmail.com>
2022-09-26 08:35:37 +0200

    fix cli options

--- bin/vodtwitch.js
+++ bin/vodtwitch.js
@@ -4,20 +4,26 @@ import downloadTwitchVod from "../index.js";
 import fs from "fs/promises";
 
 async function main() {
-  const pkg = JSON.parse(await fs.readFile("../package.json", "utf-8"));
+  const pkg = JSON.parse(await fs.readFile("./package.json", "utf-8"));
   const vodtwitch = new Command();
   vodtwitch
     .version(pkg.version, "-v, --version", "output the current version")
     .option("-o, --output <path>", "destination directory path")
     .option("-s, --silent", "disable log output")
-    .option("-t, --token", "add oauth token to request for private vods")
+    .option(
+      "-t, --token <token>",
+      "add oauth token to request for private vods"
+    )
     .parse(process.argv);
 
   const [vodIdOrUrl] = vodtwitch.args;
+
+  const { silent, outputDir, token } = vodtwitch.opts();
+
   await downloadTwitchVod(vodIdOrUrl, {
-    log: !vodtwitch.silent,
-    outputDir: vodtwitch.output,
-    oauthToken: vodtwitch.token,
+    log: !silent,
+    outputDir,
+    oauthToken: token,
   });
 }
 
